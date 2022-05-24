42b9779c49071235e032f7a67ca22b4fe7557846
Administrator <varshaankam225@gmail.com>
2022-05-24 17:42:12 +0530

    commited

--- project1/program2.cs
+++ project1/program2.cs
@@ -8,7 +8,7 @@ namespace project1
 {
     internal class program2
     {
-        static void Main(String[] args)
+        static void Main3(String[] args)
         {
             int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
            // for(int i=0; i<= numbers.Length; i++)
---
+++ project1/program3methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program3methods
+
+    {
+        static double Area(int r)
+        {
+            double output = 3.14 * r * r;
+            return output;
+        }
+        public static void Main(String[] args)
+        {
+            double res = Program3methods.Area(1);
+            Console.WriteLine(res);
+
+            double re = Program3methods.Area(2);
+            Console.WriteLine(re);
+        }
+    }
+}
